4f98347cc067d7569780f4c4435e800447e2afcf
Klaudusia12 <121953763+Klaudusia12@users.noreply.github.com>
2024-05-15 17:57:35 +0200

    asd

--- nerka/IM_2024_nerka/IM_2024_nerka/modules/analysis/main.py
+++ nerka/IM_2024_nerka/IM_2024_nerka/modules/analysis/main.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 
@@ -8,10 +7,12 @@ if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
 
+from modules.common.src.app_utils.Logger import get_logger
+from modules.common.src.app_utils.Reader import Reader, get_readers
 
 
 dirs = Reader.filter_data_folders_by_type(Reader.DirType.DIR_TYPE_SPECIMEN)
 readers = get_readers(dirs, to_left=['P01', 'P02', 'P03'])
-    for reader in readers:
-        dag = reader.load_data(Reader.DataStep.DAG_WITH_STATS_FILENAME)
-        get_logger().debug(dag.get_shape())
+for reader in readers:
+    dag = reader.load_data(Reader.DataStep.DAG_WITH_STATS_FILENAME)
+    get_logger().debug(dag.get_shape())
