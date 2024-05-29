cb8ce83302ebf39c0b4d531ccc9011e8ee6f4399
Klaudusia12 <121953763+Klaudusia12@users.noreply.github.com>
2024-05-29 13:49:16 +0200

    kl

--- nerka/IM_2024_nerka/IM_2024_nerka/modules/analysis/main.py
+++ nerka/IM_2024_nerka/IM_2024_nerka/modules/analysis/main.py
@@ -1,15 +1,12 @@
 import sys
-import os
 
 # Ustawienie ścieżki do katalogu głównego projektu
 project_root = r"C:\Users\KlaudiaDyl\Documents\GitHub\infa12\nerka\IM_2024_nerka\IM_2024_nerka"
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-
 from modules.common.src.app_utils.Logger import get_logger
 from modules.common.src.app_utils.Reader import Reader, get_readers
-
 
 dirs = Reader.filter_data_folders_by_type(Reader.DirType.DIR_TYPE_SPECIMEN)
 readers = get_readers(dirs, to_left=['P01', 'P02', 'P03'])
